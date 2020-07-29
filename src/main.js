5ccaf8f65d0128c6d37a4072339d4b48d4041a1a
evgeniy <not-found@inbox.ru>
2020-07-30 02:23:28 +0300

    Добавил функцию вставки компонентов в DOM

--- src/main.js
+++ src/main.js
@@ -369,3 +369,7 @@ const createLoadMoreButtonTemplate = () => {
     `<button class="load-more" type="button">load more</button>`
   )
 }
+
+const render = (container, template, place) => {
+  container.insertAdjacentHTML(place,template);
+}
