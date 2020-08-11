a34065bb675750112db1d19b3083665c1a87a7ed
evgeniy <not-found@inbox.ru>
2020-08-11 21:16:54 +0300

    Добавил функции для генерации моковых данных для компонента 'задача'

---
+++ src/mock/task.js
@@ -0,0 +1,69 @@
+// Функция из интернета по генерации случайного числа из диапазона
+// Источник - https://github.com/you-dont-need/You-Dont-Need-Lodash-Underscore#_random
+const getRandomInteger = (a = 0, b = 1) => {
+  const lower = Math.ceil(Math.min(a, b));
+  const upper = Math.floor(Math.max(a, b));
+
+  return Math.floor(lower + Math.random() * (upper - lower + 1));
+};
+
+const generateDescription = () => {
+  const descriptions = [
+    `Изучить теорию`,
+    `Сделать домашку`,
+    `Пройти интенсив на соточку`,
+  ];
+  const randomIndex = getRandomInteger(0, descriptions.length - 1);
+  return (descriptions[randomIndex]);
+};
+
+const generateDate = () => {
+  const isDate = Boolean(getRandomInteger(0, 1));
+
+  if (!isDate) {
+    return null;
+  }
+
+  const maxDaysGap = 7;
+  const daysGap = getRandomInteger(-maxDaysGap, maxDaysGap);
+  const currentDate = new Date();
+
+  currentDate.setHours(23, 59, 59, 999);
+  currentDate.setDate(currentDate.getDate() + daysGap);
+
+  return new Date(currentDate);
+};
+
+const generateColor = () => {
+  const colors = [
+    `black`,
+    `yellow`,
+    `blue`,
+    `green`,
+    `pink`,
+  ];
+  const randomIndex = getRandomInteger(0, colors.length - 1);
+  return (colors[randomIndex]);
+};
+
+
+export const generateTask = () => {
+  const dueDate = generateDate();
+
+  return {
+    discription: generateDescription(),
+    dueDate,
+    repeatingDays: {
+      mo: false,
+      tu: false,
+      we: false,
+      th: false,
+      fr: false,
+      sa: false,
+      su: false,
+    },
+    color: generateColor(),
+    isFavorite: false,
+    isAchive: false,
+  };
+};
