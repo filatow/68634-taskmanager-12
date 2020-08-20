c102add213c1dc577c0ec32c069ce451e330f992
evgeniy <not-found@inbox.ru>
2020-08-20 23:58:26 +0300

    Добавлено скрытие формы редактирования по клавише Escape

--- src/main.js
+++ src/main.js
@@ -31,13 +31,23 @@ const renderTask = (taskListElement, task) => {
     taskListElement.replaceChild(taskComponent.element, taskEditComponent.element);
   };
 
+  const onEscKeyDown = (event) => {
+    if (event.key === `Escape` || event.key === `Esc`) {
+      event.preventDefault();
+      replaceFormToCard();
+      document.removeEventListener(`keydown`, onEscKeyDown);
+    }
+  };
+
   taskComponent.element.querySelector(`.card__btn--edit`).addEventListener(`click`, () => {
     replaceCardToForm();
+    document.addEventListener(`keydown`, onEscKeyDown);
   });
 
   taskEditComponent.element.querySelector(`form`).addEventListener(`submit`, (event) => {
     event.preventDefault();
     replaceFormToCard();
+    document.removeEventListener(`keydown`, onEscKeyDown);
   });
 
   render(taskListElement, taskComponent.element, RenderPosition.BEFOREEND);
