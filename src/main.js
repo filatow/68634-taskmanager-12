af938400970953741d56800e7fde91a874ffe688
evgeniy <not-found@inbox.ru>
2020-08-21 00:51:13 +0300

    Рефакторинг кода: добавлена функция отрисовки доски

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import FiltersView from "./view/filters.js";
 import BoardView from "./view/board.js";
 import TaskListView from "./view/task-list.js";
 import SortingView from "./view/sorting.js";
+import NoTasksView from "./view/no-tasks.js";
 import TaskView from "./view/task.js";
 import TaskEditView from "./view/task-edit.js";
 import LoadMoreButtonView from "./view/load-more-button.js";
@@ -11,7 +12,7 @@ import {generateFilter} from "./mock/filters.js";
 import {render, RenderPosition} from "./utils.js";
 
 
-const TASK_COUNT = 30;
+const TASK_COUNT = 20;
 const TASK_COUNT_PER_STER = 8;
 
 const tasks = new Array(TASK_COUNT).fill().map(generateTask);
@@ -53,38 +54,47 @@ const renderTask = (taskListElement, task) => {
   render(taskListElement, taskComponent.element, RenderPosition.BEFOREEND);
 };
 
+const renderBoard = (boardContainer, boardTasks) => {
+  const boardComponent = new BoardView();
+  render(boardContainer, boardComponent.element, RenderPosition.BEFOREEND);
+
+  if (boardTasks.every((task) => task.isArchive)) {
+    render(boardComponent.element, new NoTasksView().element, RenderPosition.AFTERBEGIN);
+    return;
+  }
+
+  render(boardComponent.element, new SortingView().element, RenderPosition.AFTERBEGIN);
+
+  const taskListComponent = new TaskListView();
+  render(boardComponent.element, taskListComponent.element, RenderPosition.BEFOREEND);
+
+  for (let i = 0; i < Math.min(boardTasks.length, TASK_COUNT_PER_STER); i++) {
+    renderTask(taskListComponent.element, boardTasks[i]);
+  }
+
+  if (boardTasks.length > TASK_COUNT_PER_STER) {
+    let renderedTasksCount = TASK_COUNT_PER_STER;
+
+    const loadMoreButtonComponent = new LoadMoreButtonView();
+    render(boardComponent.element, loadMoreButtonComponent.element, RenderPosition.BEFOREEND);
+
+    loadMoreButtonComponent.element.addEventListener(`click`, (event) => {
+      event.preventDefault();
+      boardTasks
+        .slice(renderedTasksCount, renderedTasksCount + TASK_COUNT_PER_STER)
+        .forEach((boardTask) => renderTask(taskListComponent.element, boardTask));
+
+      renderedTasksCount += TASK_COUNT_PER_STER;
+
+      if (renderedTasksCount > boardTasks.length) {
+        loadMoreButtonComponent.element.remove();
+        loadMoreButtonComponent.removeElement();
+      }
+    });
+  }
+};
+
 render(siteHeaderElement, new SiteMenuView().element, RenderPosition.BEFOREEND);
 render(siteMainElement, new FiltersView(filters).element, RenderPosition.BEFOREEND);
 
-const boardComponent = new BoardView();
-render(siteMainElement, boardComponent.element, RenderPosition.BEFOREEND);
-
-const taskListComponent = new TaskListView();
-render(boardComponent.element, taskListComponent.element, RenderPosition.BEFOREEND);
-
-render(boardComponent.element, new SortingView().element, RenderPosition.AFTERBEGIN);
-
-for (let i = 0; i < Math.min(tasks.length, TASK_COUNT_PER_STER); i++) {
-  renderTask(taskListComponent.element, tasks[i]);
-}
-
-if (tasks.length > TASK_COUNT_PER_STER) {
-  let renderedTasksCount = TASK_COUNT_PER_STER;
-
-  const loadMoreButtonComponent = new LoadMoreButtonView();
-  render(boardComponent.element, loadMoreButtonComponent.element, RenderPosition.BEFOREEND);
-
-  loadMoreButtonComponent.element.addEventListener(`click`, (event) => {
-    event.preventDefault();
-    tasks
-      .slice(renderedTasksCount, renderedTasksCount + TASK_COUNT_PER_STER)
-      .forEach((task) => renderTask(taskListComponent.element, task));
-
-    renderedTasksCount += TASK_COUNT_PER_STER;
-    if (renderedTasksCount > tasks.length) {
-      loadMoreButtonComponent.element.remove();
-      loadMoreButtonComponent.removeElement();
-
-    }
-  });
-}
+renderBoard(siteMainElement, tasks);
