0b8054c72d0d09224203915bfcbcd83269850c5d
evgeniy <not-found@inbox.ru>
2020-08-27 23:41:46 +0300

    Правки шаблона сортировки

--- src/view/sorting.js
+++ src/view/sorting.js
@@ -1,11 +1,12 @@
 import AbstractView from "./abstract.js";
+import {SortType} from "../consts";
 
 const createSortingTemplate = () => {
   return (
     `<div class="board__filter-list">
-    <a href="#" class="board__filter" data-sort-type="default">SORT BY DEFAULT</a>
-    <a href="#" class="board__filter" data-sort-type="date-up">SORT BY DATE up</a>
-    <a href="#" class="board__filter" data-sort-type="date-down">SORT BY DATE down</a>
+    <a href="#" class="board__filter" data-sort-type="${SortType.DEFAULT}">SORT BY DEFAULT</a>
+    <a href="#" class="board__filter" data-sort-type="${SortType.DATE_UP}">SORT BY DATE up</a>
+    <a href="#" class="board__filter" data-sort-type="${SortType.DATE_DOWN}">SORT BY DATE down</a>
   </div>`
   );
 };
